9ef4ada09b7de3b99dde795021a8b7099c7d069f
kevinfASC6 <kevinfbths@gmail.com>
2019-07-13 11:26:41 -0400

    edited MAsh game

--- MASH.js
+++ MASH.js
@@ -1,8 +1,10 @@
+// VERSION 1//
 function mash() { 
-    return "house";  
-} 
+    return "house";   
+    } 
+    //console.log(mash());  
 
-
+// VERSION 2// 
 function getHome() {
     var homes = ["Mansion", "Apartment", "Shack", "House", "Graveyard"]; 
     var a = (Math.floor(Math.random()*homes.length));
@@ -13,27 +15,6 @@ function getHome() {
 function getChildrenCount() { 
     return Math.floor(Math.random()*101);   
    } 
-    
-   
-   var c = Math.random() 
-   if(c > 0.5) { 
-       console.log(process.argv[2]);  
-       console.log("You will live in a " + getHome() + ", and you will have " + getChildrenCount() + " kids!");  
-       } 
-       else{ 
-           console.log(process.argv[2]); 
-           console.log("You will live in a " + process.argv[3] + ", and you will have " + getChildrenCount() + " kids!"); 
-       } 
-
-   var c = Math.random() 
-   if(c > 0.5) { 
-       console.log(process.argv[2]);  
-       console.log("You will live in a " + getHome() + ", and you will have " + getChildrenCount() + " kids!");  
-       } 
-       else{ 
-           console.log(process.argv[2]); 
-           console.log("You will live in a " + getHome() + ", and you will have " + process.argv[4] + " kids!"); 
-       }
 
    function getCar() { 
        var car = ["Lambo", "Convertible", "Cardboard box", "Dragon with eyes", "sled"];   
@@ -41,35 +22,32 @@ function getChildrenCount() {
        return car[b]; 
 
    } 
-   console.log("You will live in a " + getHome() + ", and you will have " + " kids" + ", and you will drive a " + getCar())
-    
-   var c = Math.random() 
-   if(c > 0.5) { 
-       console.log(process.argv[2]);  
-       console.log("You will live in a " + getHome() + ", and you will have " + getChildrenCount() + " kids!" + ", and you will drive a " + process.argv[5]);  
-       } 
-       else{ 
-           console.log(process.argv[2]); 
-           console.log("You will live in a " + getHome() + ", and you will have " + getChildrenCount() + " kids!" + ", and you will drive a " + getCar()); 
-       }
-      
-     function urSpouse() { 
+   
+         function urSpouse() { 
          var spouse = ["Mary", "Jenny","Penny", "Amy"]; 
          var d = (Math.floor(Math.random()*spouse.length)); 
          return spouse[d]; 
      } 
-      
-       console.log("You will live in a " + getHome() + ", and you will have " + getChildrenCount() + " kids" + ", and you will drive a " + getCar())
         
        function getJob() { 
            var job = ["Doctor", "Jobless person", "Teacher", "Gamer"]; 
            var f = (Math.floor(Math.random()*job.length));  
            return job[f]; 
+       } 
+
+       function getFriends() { 
+           var friends = ["has no friends", "has some friends", "has a lot of friends"]; 
+           var g = (Math.floor(Math.random()*friends.length)); 
+           return friends[g];
        }
        var e = Math.random() 
        if(e > 0.5) { 
-           console.log("Hey " + process.argv[2] + " you are living in a " + process.argv[3] + ", has a " + process.argv[4] + " and your a " + process.argv[5] + " who has no friends"); 
+           console.log("Hey " + process.argv[2] + " you are living in a " + process.argv[3] + ", has a " + process.argv[4] + " and your a " + process.argv[5] + " who " + getFriends()); 
          }
         else{ 
-            console.log("Hey " + process.argv[2] + " you are living in a " + getHome() + ", has a " + getCar() + " and your a " + getJob() + " who has some friends")
-        }
+            console.log("Hey " + process.argv[2] + " you are living in a " + getHome() + ", has a " + getCar() + " and your a " + getJob() + " who " + getFriends()); 
+        }    
+
+        
+
+  
